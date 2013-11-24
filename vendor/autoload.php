962d417268e56790de2da7e5b0b4954aeeffe8c2
Simon Winter <simon@saltedherring.com>
2013-11-25 12:33:28 +1300

    added ininstallers

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit867440c0554a6000ccface427fcde904::getLoader();
+return ComposerAutoloaderInit10d9748b01ed186e3321d04feb5e50bd::getLoader();
