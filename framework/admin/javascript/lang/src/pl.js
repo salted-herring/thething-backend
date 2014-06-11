992c8753d9a15cdf19ee065bda0fb6295a679c11
Simon Winter <simon@saltedherring.com>
2014-06-12 10:13:28 +1200

    Removed CMS & framework

--- framework/admin/javascript/lang/src/pl.js
+++
@@ -1,6 +0,0 @@
-{
-	"ModelAdmin.SAVED": "Zapisano",
-	"ModelAdmin.REALLYDELETE": "Napewno usunąć?",
-	"ModelAdmin.DELETED": "Usunięto",
-	"LeftAndMain.PAGEWASDELETED": "Ta strona została usunięta."
-}
--- framework/admin/javascript/lang/src/sk.js
+++
@@ -1,7 +0,0 @@
-{
-	"ModelAdmin.SAVED": "Uložené",
-	"ModelAdmin.REALLYDELETE": "Skutočně chcete zmazať?",
-	"ModelAdmin.DELETED": "Zmazané",
-	"LeftAndMain.PAGEWASDELETED": "Táto stránka bola zmazaná. Pre editáciu stránky, vyberte ju vľavo.",
-	"LeftAndMain.CONFIRMUNSAVED": "Určite chcete opustiť navigáciu z tejto stránky?\n\nUPOZORNENIE: Vaše zmeny neboli uložené.\n\nStlačte OK pre pokračovať, alebo Cancel, ostanete na teto stránke."
-}
