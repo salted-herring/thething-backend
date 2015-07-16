7cfbe4d7d163da1e5dfc5e6ab43e8e24dde7d284
Simon Winter <simon@saltedherring.com>
2015-07-16 13:50:25 +1200

    Added Social extensions for OG & twitter

--- mainsite/code/Extensions/PageExtensions.php
+++ mainsite/code/Extensions/PageExtensions.php
@@ -66,4 +66,52 @@
 			$mobi = new Mobile_Detect();
 			return $mobi->isMobile();
 		}
+		
+		/**
+		 * Retrieve all Open graph & twitter tags:
+		 * - If the page has the tags, show them.
+		 * - If not then get the site config defaults.
+		 * */
+		public function getSocialTags() {
+			$siteConfig = SiteConfig::current_site_config();
+			$data = array();
+			
+			foreach(array('TitterCard', 'TwitterTitle', 'TwitterDescription') as $tw) {
+				if ($this->owner->$tw) {
+					$data[$tw] = $this->owner->$tw;
+				} else {
+					if ($siteConfig->$tw) {
+						$data[$tw] = $siteConfig->$tw;
+					}
+				}
+			}
+			
+			if ($this->owner->TwitterImageCropped()->exists() || $siteConfig->TwitterImageCropped()->exists()) {
+				if ($this->owner->TwitterImageCropped()->exists() && $this->owner->TwitterImage()->exists()) {
+					$data['TwitterImage'] = $this->owner->TwitterImageCropped()->AbsoluteURL;
+				} elseif ($siteConfig->TwitterImageCropped()->exists() && $siteConfig->TwitterImage()->exists()) {
+					$data['TwitterImage'] = $siteConfig->TwitterImageCropped()->AbsoluteURL;
+				}
+			}
+			
+			foreach(array('OGType', 'OGTitle', 'OGDescription') as $tw) {
+				if ($this->owner->$tw) {
+					$data[$tw] = $this->owner->$tw;
+				} else {
+					if ($siteConfig->$tw) {
+						$data[$tw] = $siteConfig->$tw;
+					}
+				}
+			}
+			
+			if ($this->owner->OGImageCropped()->exists() || $siteConfig->OGImageCropped()->exists()) {
+				if ($this->owner->OGImageCropped()->exists() && $this->owner->OGImage()->exists()) {
+					$data['OGImage'] = $this->owner->OGImageCropped()->AbsoluteURL;
+				} elseif ($siteConfig->OGImageCropped()->exists() && $siteConfig->OGImage()->exists()) {
+					$data['OGImage'] = $siteConfig->OGImageCropped()->AbsoluteURL;
+				}
+			}
+			
+			return Controller::curr()->customise(new ArrayData($data))->renderWith("OG");
+		}
 	}
